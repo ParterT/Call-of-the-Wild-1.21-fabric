1ed145a2ae694da011b06a4801e811ae352d3d95
ParterT <gad59922@gmail.com>
2025-02-21 22:44:09 +0300

    Initial commit

---
+++ src/main/java/net/partert/call_wild/CallOfTheWild.java
@@ -0,0 +1,17 @@
+package net.partert.call_wild;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CallOfTheWild implements ModInitializer {
+	public static final String MOD_ID = "call_wild";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/partert/call_wild/CallOfTheWildClient.java
@@ -0,0 +1,10 @@
+package net.partert.call_wild;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CallOfTheWildClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
